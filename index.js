560080233fc18b6db1ab530ee2adab9c60d4ab3e
Jueun Jeon <jjeon02@risd.edu>
2020-03-09 00:53:21 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head lang="en">
+        <meta http-equiv="X-UA-Compatible" content = "ie=edge">
+        <meta charset="utf-8"> <!-- able multiple language -->
+        <meta name="description" content="">
+        <meta name ="viewport" content="width=device-width">
+        <title>webtype_2020</title>
+    <body>
+       
+        Hello World
+
+        <ul>
+            <li>
+                <a href="project_1/index.html" target="_blank"> Project 1</a>
+            </li>
+            <li>Project 2</li>
+            <li>Project 3</li>
+            <li>Project 4</li>
+        </ul>
+        Practice
+        <ul>
+            <li>
+                <a href="practice/0220.html" target="_blank">0220</a>
+            </li>
+        </ul>
+    </body>
+</html>
