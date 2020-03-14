31b9193d303637162b20d542ce9b20637155f05d
Jueun Jeon <jjeon02@risd.edu>
2020-03-14 00:50:47 -0400

    JS

--- project_2/index.js
+++ project_2/index.js
@@ -1,3 +1,29 @@
-function setup (){
-    noCanvas();
+
+window.onload = function() {
+    countUpFromTime("Jan 1, 1894 12:00:00"); //
+};
+
+// timemachine
+function countUpFromTime(countFrom, id) {
+    countFrom = new Date(countFrom).getTime();
+    var now = new Date(),
+        countFrom = new Date(countFrom),
+        timeDifference = (now - countFrom);
+    
+    var secondsInADay = 60 * 60 * 1000 * 24,
+        secondsInAHour = 60 * 60 * 1000;
+    
+    days = Math.floor(timeDifference / (secondsInADay) * 1);
+    years = Math.floor(days / 365);
+    if (years > 1){ days = days - (years * 365) }
+    hours = Math.floor((timeDifference % (secondsInADay)) / (secondsInAHour) * 1);
+    mins = Math.floor(((timeDifference % (secondsInADay)) % (secondsInAHour)) / (60 * 1000) * 1);
+    secs = Math.floor((((timeDifference % (secondsInADay)) % (secondsInAHour)) % (60 * 1000)) / 1000 * 1);
+
+    document.getElementById('timer').firstChild.nodeValue= years+' '+':'+' '+days+' '+':'+' '+hours+' '+':'+' '+mins+' '+':'+' '+secs;
+
+    clearTimeout(countUpFromTime.interval);
+    countUpFromTime.interval = setTimeout(function(){ countUpFromTime(countFrom, id);}, 1000);
 }
+
+
