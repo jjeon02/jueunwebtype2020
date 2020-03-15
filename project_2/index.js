7c8e93b7678a08a73609f1872fd2ec02ef1b1462
Jueun Jeon <jjeon02@risd.edu>
2020-03-15 15:53:21 -0400

    js

--- project_2/index.js
+++ project_2/index.js
@@ -1,14 +1,13 @@
 
-window.onload = function() {
-    countUpFromTime("Jan 1, 1894 12:00:00"); //
+window.onload = function(){
+    countUp("Jan 1, 1894 12:00:00",'timer');
 };
 
-// timemachine
-function countUpFromTime(countFrom, id) {
+function countUp(countFrom, id){
     countFrom = new Date(countFrom).getTime();
     var now = new Date(),
-        countFrom = new Date(countFrom),
-        timeDifference = (now - countFrom);
+            countFrom = new Date(countFrom),
+            timeDifference = (now - countFrom);
     
     var secondsInADay = 60 * 60 * 1000 * 24,
         secondsInAHour = 60 * 60 * 1000;
@@ -20,10 +19,10 @@ function countUpFromTime(countFrom, id) {
     mins = Math.floor(((timeDifference % (secondsInADay)) % (secondsInAHour)) / (60 * 1000) * 1);
     secs = Math.floor((((timeDifference % (secondsInADay)) % (secondsInAHour)) % (60 * 1000)) / 1000 * 1);
 
-    document.getElementById('timer').firstChild.nodeValue= years+' '+':'+' '+days+' '+':'+' '+hours+' '+':'+' '+mins+' '+':'+' '+secs;
+    document.getElementById(id).innerHTML = years +':'+days+':'+ hours+':'+ mins +':'+secs;
 
-    clearTimeout(countUpFromTime.interval);
-    countUpFromTime.interval = setTimeout(function(){ countUpFromTime(countFrom, id);}, 1000);
+    clearTimeout(countUp.interval);
+    countUp.interval = setTimeout(function(){ countUp(countFrom, id);}, 1000);
+
 }
-
 
