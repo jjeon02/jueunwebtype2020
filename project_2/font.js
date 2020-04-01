631ddc3557922232010dff734e0775eec0f3c10d
Jueun Jeon <jjeon02@risd.edu>
2020-04-02 08:40:44 +0900

    changefonthtml

---
+++ project_2/font.js
@@ -0,0 +1,9 @@
+function changeWeight() {
+    var fon = document.getElementById("change");
+    if (fon.className == "light") { 
+        fon.className = 'bold';
+    }
+    else{
+        fon.className='light';
+    }
+}
